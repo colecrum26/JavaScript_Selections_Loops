67a7836412d2690207332880edff8c6846607e83
colecrum26 <cwcrumrine@gmail.com>
2023-03-14 14:46:13 -0500

    exercises 1&2

--- app.js
+++ app.js
@@ -2,6 +2,21 @@ console.log("Hello World!\n==========\n");
 
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
+for (var i = 1; i <= 100; i++) {
+  if (i % 2 == 0);
+  else {
+    console.log(i);
+  }
+}
 
 // Exercise 2 Section
 console.log("EXERCISE 2:\n==========\n");
+for (var i = 1; i <= 100; i++) {
+  if (i % 3 == 0 && i % 5 == 0) {
+    console.log("FIZZBUZZ");
+  } else if (i % 3 == 0) {
+    console.log("FIZZ");
+  } else if (i % 5 == 0) {
+    console.log("BUZZ");
+  }
+}
